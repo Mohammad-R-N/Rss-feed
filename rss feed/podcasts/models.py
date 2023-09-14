06dc959e5d5653bf284734c1d263906dc2cc39a4
mohammad-r-n <mohammad.ca@proton.me>
2023-09-14 19:40:03 -0400

    chore:XML model

--- rss feed/podcasts/models.py
+++ rss feed/podcasts/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=70)
+
+
+class XML(models.Model):
+    link = models.URLField(max_length=255, unique=True)
+    name = models.CharField(max_length=50, unique=True, default="Untitled")
+
+    def __str__(self):
+        return self.name
