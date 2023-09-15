2aafc27ec92de67792812c8877667a80b973784b
mohammad-r-n <mohammad.ca@proton.me>
2023-09-14 20:02:42 -0400

    chore:Episode model

--- rss feed/podcasts/models.py
+++ rss feed/podcasts/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from accounts.models import Follow
 
 class Category(models.Model):
+
     name = models.CharField(max_length=70)
 
 
 class XML(models.Model):
+
     link = models.URLField(max_length=255, unique=True)
     name = models.CharField(max_length=50, unique=True, default="Untitled")
 
@@ -14,6 +16,7 @@ class XML(models.Model):
     
 
 class Channel(models.Model):
+
     title = models.CharField(max_length=100)
     description=models.TextField(null=True, blank=True)
     language = models.CharField(max_length=5, null=True, blank=True)
@@ -29,6 +32,29 @@ class Channel(models.Model):
     xml=models.ForeignKey(XML,on_delete=models.PROTECT)
     follow=models.ForeignKey(Follow,on_delete=models.PROTECT)
     category=models.ForeignKey(Category,on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+    
+
+class Episode(models.Model):
+    
+    title = models.CharField(max_length=50)
+    link=models.URLField(max_length=255, null=True, blank=True)
+    guid=models.CharField(max_length=60)
+    pubdate = models.CharField(max_length=100)
+    author = models.CharField(max_length=50, null=True)
+    enclosure=models.URLField(max_length=255, null=True, blank=True)
+    subtitle=models.TextField()
+    duration = models.CharField(max_length=25)
+    explicit = models.CharField(max_length=5, default="no")
+    description = models.TextField(null=True, blank=True)
+    keywords=models.TextField(null=True, blank=True)
+    encoded=models.TextField()
+    summary = models.TextField(null=True, blank=True)
+    player = models.URLField(max_length=255)
+    image = models.URLField(max_length=255, null=True, blank=True)
+    channel = models.ForeignKey(Channel, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
