9c8685db8f0d303572302062a744220f3dc0e74d
mohammad-r-n <mohammad.ca@proton.me>
2023-09-14 20:36:54 -0400

    chore:LogIn API

--- rss feed/API/views/account_views.py
+++ rss feed/API/views/account_views.py
@@ -16,3 +16,25 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+
+app_name="API"
+class LoginAPIView(APIView):
+    def post(self, request):
+        account = Account.objects.filter(email=request.data["email"]).first()
+
+        if not account:
+            raise APIException("Invalid credentials!")
+
+        if not account.check_password(request.data["password"]):
+            raise APIException("Invalid credentials!")
+
+        access_token = create_access_token(account.id)
+        refresh_token = create_refresh_token(account.id)
+
+        response = Response()
+
+        response.set_cookie(key="refreshToken", value=refresh_token, httponly=True)
+        response.data = {"token": access_token}
+
+        return response
