fa00661905b1b6dc7033ee349649291bc0694369
mohammad-r-n <mohammad.ca@proton.me>
2023-09-14 20:35:39 -0400

    chore: urls updated

--- rss feed/config/urls.py
+++ rss feed/config/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('accounts/',include('accounts.urls',namespace="accounts")),
+    path('API/',include('API.urls',namespace="API")),
     path('podcasts/',include('podcasts.urls',namespace="podcasts")),
 ]
