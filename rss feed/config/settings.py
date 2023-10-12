b09362a62f28b2d8be2a565d692af1e3fb806c4a
mohammad-r-n <mohammad.ca@proton.me>
2023-10-12 04:12:13 -0400

    delete API app from INSTALLED APPS

--- rss feed/config/settings.py
+++ rss feed/config/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'API.apps.ApiConfig',
     'accounts.apps.AccountsConfig',
     'podcasts.apps.PodcastsConfig',
 ]
