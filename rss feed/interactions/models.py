4dd394f30d9909cc47720d5560f1d1d66c56c704
mohammad-r-n <mohammad.ca@proton.me>
2023-10-12 07:50:23 -0400

    chore: Like model added

--- rss feed/interactions/models.py
+++ rss feed/interactions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from config import settings
+from core.models import BaseModel
 
-# Create your models here.
+
+class Like(BaseModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
